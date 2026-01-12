19708cd4b49cb3c1f156f0e67d9f8655db99e191
Justin Frandsen <jfran4047@gmail.com>
2026-01-12 14:51:17 -0600

    added analyses

--- fire_lme.R
+++ fire_lme.R
@@ -26,7 +26,14 @@ fixation_features <- fixation_features %>%
 # full model with all predictors
 ## relevel to compare background and vegitation vs fire
 fixation_features$roi_type <- relevel(fixation_features$roi_type, ref = "1")
-fixation_features %>% filter()
+
+fixation_features <- fixation_features %>%
+  group_by(SubjectName) %>%
+  mutate(
+    trial_num = cumsum(Stimuli != lag(Stimuli, default = first(Stimuli))) + 1
+  ) %>%
+  ungroup()
+
 
 #model
 model_fix_full <- lmer(FixDur ~ saliency_scaled + area_scaled + roi_type + (1 | SubjectName),
@@ -51,19 +58,51 @@ summary(model_no_first)
 
 # Trial Level Models
 trial_df <- fixation_features %>%
-  group_by(SubjectName, Species, Stimuli, Trial) %>%
-  summarise(mean_fixdur   = mean(FixDur),
-            median_fixdur = median(FixDur),
-            mean_sal      = mean(saliency_scaled),
-            mean_area     = mean(area_scaled),
-            prop_fire     = mean(as.numeric(as.character(roi_type))), # proportion fixations on fire
-            n_fix         = n(),
-            .groups = "drop")
+  group_by(SubjectName, Species, Stimuli, trial_num) %>%
+  summarise(
+    total_time = sum(FixDur),
+    
+    fire_time = sum(FixDur[roi_type == "1"]),
+    veg_time  = sum(FixDur[roi_type == "2"]),
+    bg_time   = sum(FixDur[roi_type == "0"]),
+    
+    prop_time_fire = fire_time / total_time,
+    
+    fire_sal = mean(saliency_scaled[roi_type == "1"]),
+    veg_sal  = mean(saliency_scaled[roi_type == "2"]),
+    bg_sal   = mean(saliency_scaled[roi_type == "0"]),
+    
+    sal_diff = fire_sal - (veg_sal + bg_sal),
+    
+    fire_area = mean(area_scaled[roi_type == "1"]),
+    veg_area  = mean(area_scaled[roi_type == "2"]),
+    bg_area   = mean(area_scaled[roi_type == "0"]),
+    .groups = "drop"
+  )
 
-model_trial <- lmer(median_fixdur ~ mean_sal + mean_area + prop_fire * Species + (1 | SubjectName),
-                    data = trial_df)
+
+
+model_trial <- lmer(prop_time_fire ~ sal_diff + fire_area + Species + (1 | SubjectName), data = trial_df)
+
 
 summary(model_trial)
+
+trial_roi_df <- fixation_features %>%
+  group_by(SubjectName, Species, Stimuli, trial_num, roi_type) %>%
+  summarise(
+    total_fixdur = sum(FixDur),
+    mean_sal     = mean(saliency_scaled),
+    mean_area    = mean(area_scaled),
+    .groups = "drop"
+  )
+
+t2 <- lmer(
+  total_fixdur ~ roi_type + mean_sal + mean_area + Species + (1 | SubjectName),
+  data = trial_roi_df
+)
+
+summary(t2)
+
 
 #only chimp results
 fix_chimp <- fixation_features %>% filter(Species == "Chimp")
