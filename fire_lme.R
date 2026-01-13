c1724b53266f67b85daf08afd5e4442658f0b57a
Justin Frandsen <jfran4047@gmail.com>
2026-01-13 13:10:17 -0600

    fixed trial level lme

--- fire_lme.R
+++ fire_lme.R
@@ -57,52 +57,76 @@ model_no_first <- lmer(FixDur ~ saliency_scaled + area_scaled + roi_type + Speci
 summary(model_no_first)
 
 # Trial Level Models
-trial_df <- fixation_features %>%
-  group_by(SubjectName, Species, Stimuli, trial_num) %>%
-  summarise(
-    total_time = sum(FixDur),
-    
-    fire_time = sum(FixDur[roi_type == "1"]),
-    veg_time  = sum(FixDur[roi_type == "2"]),
-    bg_time   = sum(FixDur[roi_type == "0"]),
-    
-    prop_time_fire = fire_time / total_time,
-    
-    fire_sal = mean(saliency_scaled[roi_type == "1"]),
-    veg_sal  = mean(saliency_scaled[roi_type == "2"]),
-    bg_sal   = mean(saliency_scaled[roi_type == "0"]),
-    
-    sal_diff = fire_sal - (veg_sal + bg_sal),
-    
-    fire_area = mean(area_scaled[roi_type == "1"]),
-    veg_area  = mean(area_scaled[roi_type == "2"]),
-    bg_area   = mean(area_scaled[roi_type == "0"]),
-    .groups = "drop"
-  )
-
-
-
-model_trial <- lmer(prop_time_fire ~ sal_diff + fire_area + Species + (1 | SubjectName), data = trial_df)
-
-
-summary(model_trial)
-
 trial_roi_df <- fixation_features %>%
   group_by(SubjectName, Species, Stimuli, trial_num, roi_type) %>%
   summarise(
     total_fixdur = sum(FixDur),
-    mean_sal     = mean(saliency_scaled),
-    mean_area    = mean(area_scaled),
+    mean_sal     = mean(saliency),
+    mean_area    = mean(area),
     .groups = "drop"
+  ) 
+
+trial_roi_df <- fixation_features %>%
+  group_by(SubjectName, Species, Stimuli, trial_num, roi_type, ImageType) %>%
+  summarise(
+    total_fixdur = sum(FixDur),
+    mean_sal     = mean(saliency),
+    mean_area    = mean(area),
+    .groups = "drop"
+  ) %>%
+  group_by(SubjectName, Species, Stimuli, trial_num) %>%
+  complete(
+    roi_type,
+    fill = list(
+      total_fixdur = 0,
+      mean_sal = 0,
+      mean_area = 0
+    )
+  ) %>%
+  ungroup() %>% # Scale predictors after aggregation
+  mutate(
+    total_fixdur_scaled = scale(total_fixdur)[,1],
+    mean_sal_scaled     = scale(mean_sal)[,1],
+    mean_area_scaled    = scale(mean_area)[,1]
   )
 
-t2 <- lmer(
-  total_fixdur ~ roi_type + mean_sal + mean_area + Species + (1 | SubjectName),
-  data = trial_roi_df
+fixation_features$roi_type <- relevel(fixation_features$roi_type, ref = "1")
+
+model_trial <- lmer(
+  total_fixdur ~ roi_type + mean_sal_scaled + mean_area_scaled +
+    (1 | SubjectName),
+  data = trial_roi_df)
+
+summary(model_trial)
+
+trial_roi_df %>%
+  group_by(roi_type) %>%
+  summarise(
+    mean_fixdur = mean(total_fixdur),
+    sd_fixdur   = sd(total_fixdur),
+    median_fixdur = median(total_fixdur),
+    n = n()
+  )
+
+#compare with and without roi_type
+
+model_with_roi <- lmer(
+  total_fixdur ~ roi_type + mean_sal_scaled + mean_area_scaled +
+    (1 | SubjectName),
+  data = trial_roi_df,
+  REML = FALSE
 )
 
-summary(t2)
+model_no_roi <- lmer(
+  total_fixdur ~ mean_sal_scaled + mean_area_scaled +
+    (1 | SubjectName),
+  data = trial_roi_df,
+  REML = FALSE
+)
 
+anova(model_no_roi, model_with_roi)
+AIC(model_no_roi, model_with_roi)
+BIC(model_no_roi, model_with_roi)
 
 #only chimp results
 fix_chimp <- fixation_features %>% filter(Species == "Chimp")
@@ -111,7 +135,6 @@ model_chimp <- lmer(FixDur ~ saliency_scaled + area_scaled + ROI + (1 | SubjectN
                     data = fix_chimp)
 
 summary(model_chimp)
-
 #only baboon results
 fix_baboon <- fixation_features %>% filter(Species == "Baboon")
 
